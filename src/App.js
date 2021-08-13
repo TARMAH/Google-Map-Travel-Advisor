7741e00b5a4a78c252da0bea7d45016f13ebec84
Tarmah Bin Iqbal <tiqbal.bscs16seecs@seecs.edu.pk>
2021-08-13 14:45:23 +0500

    initial project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./styles.css";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+import { Grid } from "@material-ui/core";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8} style={{}}>
+          <Map />
+        </Grid>
+      </Grid>
     </div>
   );
 }
