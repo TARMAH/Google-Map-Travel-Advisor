48481127c746a46fa33b3f0b71bc970b5d609b5f
Tarmah Bin Iqbal <tiqbal.bscs16seecs@seecs.edu.pk>
2021-08-16 15:49:47 +0500

    started with list component

---
+++ src/components/List/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginBottom: "30px"
+  },
+  container: {
+    padding: "25px"
+  },
+  list: {
+    height: "65vh",
+    overflow: "auto"
+  }
+}));
